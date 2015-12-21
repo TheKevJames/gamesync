44cda10073c733df797d902a83c3249ae369da2c
Kevin James <KevinJames@thekev.in>
2015-12-21 16:34:15 -0500

    v1.1.2 version bump

--- gamesync/gamesync.py
+++ gamesync/gamesync.py
@@ -15,7 +15,7 @@ from .commands import add, backup, create_gamesync_folder, remove, status
 from .definitions import DEFINITIONS
 
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 
 def main(args=None):
